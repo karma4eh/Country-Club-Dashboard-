0fbecbc170e5359d650be40895266bb54864b8f4
Karma <Karma4eh@gmail.com>
2024-09-30 15:33:02 -0400

    new

--- js/ver_socios.js
+++ js/ver_socios.js
@@ -8,7 +8,7 @@ function verDetallesSocio(socioId) {
         .then(data => {
             // Insertar los datos en la modal
             document.getElementById('modal-content').innerHTML = `
-                <h2 class="text-2xl font-bold mb-4 text-white text-center text-yellow-500">Socio número ${data.id}</h2>
+                <h2 class="text-2xl font-bold mb-4 text-white text-center text-yellow-500">Socio ${data.id}</h2>
                 <p><strong>Nombre Completo:</strong> ${data.nombre} ${data.apellido}</p>
                 <p><strong>Dirección:</strong> ${data.direccion}</p>
                 <p><strong>Cédula:</strong> V-${data.cedula}</p>
